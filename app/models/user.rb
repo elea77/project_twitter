f8853bf325daca959a42284f954d0364c8545259
Eléa Carton <eleacarton@MacBook-Air.local>
2020-11-24 12:49:54 +0100

    session and login user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    # L'utilisateur doit pouvoir garder un mot de passer sécurisé
+    has_secure_password
+
+    # L'email ne doit pas etre nul, et doit etre unique dans toute la base
+    validates :email, presence: true,
+        uniqueness: true
 end
