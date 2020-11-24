d943d84439c30ca44de52eb1e3a5d7b08017ba94
Eléa Carton <eleacarton@MacBook-Air.local>
2020-11-24 17:40:33 +0100

    edit tweet and debug

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,74 +1,59 @@
 class TweetsController < ApplicationController
-  before_action :set_tweet, only: [:show, :edit, :update, :destroy]
+  before_action :authorize_user, only: [:new, :create, :edit, :update]
+  before_action :set_tweet, only: [:edit, :update]
+  before_action :redirect_user_if_no_tweet, only: [:edit, :update]
 
-  # GET /tweets
-  # GET /tweets.json
   def index
     @tweets = Tweet.all
   end
 
-  # GET /tweets/1
-  # GET /tweets/1.json
   def show
   end
 
-  # GET /tweets/new
   def new
     @tweet = Tweet.new
   end
 
-  # GET /tweets/1/edit
+  def create
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user = @current_user
+    if @tweet.save
+      redirect_to tweets_path
+    else
+      redirect_to 'new_tweet_path', notice: 'Wrong'
+    end
+  end
+
   def edit
   end
 
-  # POST /tweets
-  # POST /tweets.json
-  def create
-    @tweet = Tweet.new(tweet_params)
-
-    respond_to do |format|
-      if @tweet.save
-        format.html { redirect_to @tweet, notice: 'Tweet was successfully created.' }
-        format.json { render :show, status: :created, location: @tweet }
-      else
-        format.html { render :new }
-        format.json { render json: @tweet.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /tweets/1
-  # PATCH/PUT /tweets/1.json
   def update
-    respond_to do |format|
-      if @tweet.update(tweet_params)
-        format.html { redirect_to @tweet, notice: 'Tweet was successfully updated.' }
-        format.json { render :show, status: :ok, location: @tweet }
-      else
-        format.html { render :edit }
-        format.json { render json: @tweet.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /tweets/1
-  # DELETE /tweets/1.json
-  def destroy
-    @tweet.destroy
-    respond_to do |format|
-      format.html { redirect_to tweets_url, notice: 'Tweet was successfully destroyed.' }
-      format.json { head :no_content }
+    if @tweet.update(tweet_params)
+      redirect_to 'tweets_path'
+    else
+      redirect_to tweet_path(@tweet), notice: 'Wrong'
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_tweet
-      @tweet = Tweet.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def tweet_params
-      params.require(:tweet).permit(:content, :user_id)
+  def set_tweet
+    @tweet = Tweet.find_by(id: params[:id])
+  end
+
+  def authorize_user
+    if @current_user.nil?
+      redirect_to sign_in_path
     end
+  end
+
+  def redirect_user_if_no_tweet
+    if @tweet.nil? || @current_user.id != @tweet.user.id
+      redirect_to tweets_path
+    end
+  end
+
+  def tweet_params
+    params.require(:tweet).permit(:content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  resources :tweets
   resources :users
-  get '/users', to: 'users#all'
-  get '/users/:id', to: 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get '/sign_in', to: 'sessions#sign_in_form'
-  post '/sign_in', to: 'sessions#sign_in'
-  delete '/sign_out', to: 'sessions#sign_out'
+  get    '/sign_in',    to: 'sessions#sign_in_form'
+  post   '/sign_in',    to: 'sessions#sign_in'
+  delete '/sign_out',   to: 'sessions#sign_out'
+
+  get    '/tweets',     to: 'tweets#index'
+  get    '/tweets/new', to: 'tweets#new', as: 'new_tweet'
+  post   '/tweets',     to: 'tweets#create'
+  get    '/tweets/:id/edit', to: 'tweets#edit', as: 'tweet'
+  post   '/tweets/:id/edit', to: 'tweets#update'
 end
